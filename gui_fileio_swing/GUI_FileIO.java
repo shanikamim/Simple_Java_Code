21990f65ca7cb3bff2fdb5991b48031302788391
shanikamim <50496921+shanikamim@users.noreply.github.com>
2020-01-23 13:17:54 +0600

    Add files via upload

---
+++ gui_fileio_swing/GUI_FileIO.java
@@ -0,0 +1,15 @@
+
+package gui_fileio;
+
+/**
+ *
+ * @author Shanzida Anika Mim
+ */
+public class GUI_FileIO {
+
+    public static void main(String[] args) {
+        View view=new View();
+        view.show();
+    }
+    
+}
